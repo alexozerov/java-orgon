2ee29347d859de35351593f8fd8c4ca8680ee287
alexozerov <aozerov@gmail.com>
2025-03-17 14:32:52 +0300

    MAX_ACTIVE_WITNESS_NUM = 31

--- common/src/main/java/org/tron/core/config/Parameter.java
+++ common/src/main/java/org/tron/core/config/Parameter.java
@@ -58,11 +58,11 @@ public class Parameter {
 
   public class ChainConstant {
 
-    public static final int MAX_ACTIVE_WITNESS_NUM = 30;
-    public static final int WITNESS_STANDBY_LENGTH = 300;
+    public static final int MAX_ACTIVE_WITNESS_NUM = 31;
+    public static final int WITNESS_STANDBY_LENGTH = 301;
     public static final long TRANSFER_FEE = 0; // free
     public static final int BLOCK_PRODUCED_INTERVAL = 9000; //ms,produce block period, must be divisible by 60. millisecond
-    public static final int MAX_VOTE_NUMBER = 30;
+    public static final int MAX_VOTE_NUMBER = 34;
     public static final int SOLIDIFIED_THRESHOLD = 70; // 70%
     public static final int PRIVATE_KEY_LENGTH = 64;
     public static final int BLOCK_SIZE = 2_000_000;
